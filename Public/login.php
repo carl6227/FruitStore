d00673847757ec06f24e96f2821e5391f4148eb3
genevarivas <geneva.rivas@student.passerellesnumeriques.org>
2021-05-03 15:32:51 +0800

    log in message fixed

--- Public/login.php
+++ Public/login.php
@@ -27,14 +27,14 @@ $mystore->signup();
       <div class="loginMsg">
         <div class="textcontent">
           <p class="title">Don't have an account?</p>
-          <p>Sign up to save all your graph.</p>
+          <p>Sign up to enjoy our products.</p>
           <button id="switch1">Sign Up</button>
         </div>
       </div>
       <div class="signupMsg visibility">
         <div class="textcontent">
           <p class="title">Have an account?</p>
-          <p>Log in to see all your collection.</p>
+          <p>Log in and start shoppingg.</p>
           <button id="switch2">LOG IN</button>
         </div>
       </div>
